71c7ad3e3ac28883ed4bdd3d7151bb1a9946a54a
neptune-spear <69664669+neptune-spear@users.noreply.github.com>
2022-11-11 20:00:35 -0500

    updates

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-@authors: Tyler Bokey, Joe Hibbler, Shrey Shah
+@authors: Tyler B, Joe H, Shrey S
 """
 
 import os
@@ -15,68 +15,125 @@ app.debug = True
 #mulltiple pages are part of this website
 @app.route('/', methods=["POST", "GET"]) #
 def index():
-    """ This function is designed as the root page for the WebApplication"""
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """    
     return render_template("home_page.html")
 
 @app.route('/home_page', methods=["POST", "GET"])
 def home_page():
-    """ DESCRIPTION HERE """
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """    
     return render_template('home_page.html')
 
 @app.route('/sign_up', methods=["POST", "GET"])
 def sign_up():
-    """ This function will be used for designing the login html webpage,
-    so the user can have access to the entirety of the website. """
+    """This function will be used for designing the login html webpage,
+    so the user can have access to the entirety of the website.
+
+    Returns:
+        _type_: _description_
+    """    
     return render_template("sign_up.html")
 
 #creates login form, so the user can have the ability to login
 @app.route('/login_form', methods=["POST", "GET"])
 def login_form():
-    """ This function will be used for designing the login html webpage,
-    so the user can have access to the entirety of the website. """
+    """This function will be used for designing the login html webpage,
+    so the user can have access to the entirety of the website.
+
+    Returns:
+        _type_: _description_
+    """    
     return render_template("login_form.html")
 
 @app.route("/logout_page", methods=["POST", "GET"])
 def logout_page():
-    """ Serves as the logout webpage """
+    """Serves as the logout webpage
+
+    Returns:
+        _type_: _description_
+    """    
     return render_template('logout_page.html')
 
 #Creates registration form for a new user to register account
 @app.route("/register_for_camp", methods=["POST", "GET"])
 def register_for_camp():
-    """ Serving the Registration.html webpage for user to sign up in case
-    if they are not logged in or do not have access """
+    """Serving the Registration.html webpage for user to sign up in case
+    if they are not logged in or do not have access
+
+    Returns:
+        _type_: _description_
+    """    
     return render_template('checkout_form.html')
 
 @app.route('/events', methods=["POST", "GET"])
 def events():
-    """ DESCRIPTION HERE """
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """    
     return render_template('events.html')
 
-@app.route('/Page1', methods=["POST", "GET"])
-def page1():
-    """ DESCRIPTION HERE """
-    return render_template('Training.html')
+@app.route('/staff_login', methods=["POST", "GET"])
+def staff_login():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """    
+    return render_template('staff_login.html')
+
+@app.route('/staff_dashboard', methods=["POST", "GET"])
+def staff_dashboard1():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """    
+    return render_template('staff_dashboard.html')    
 
 @app.route('/event_schedule', methods=["POST", "GET"])
 def event_schedule():
-    """ DESCRIPTION HERE """
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """    
     return render_template('event_schedule.html')
 
 @app.route('/about_us', methods=["POST", "GET"])
 def about_us():
-    """ DESCRIPTION HERE """
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """    
     return render_template('about_us.html')
 
 @app.route('/legal_notice', methods=["POST", "GET"])
 def legal_notice():
-    """ DESCRIPTION HERE """
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """    
     return render_template('legal_notice.html')
 
 @app.route('/privacy_statement', methods=["POST", "GET"])
 def privacy_statement():
-    """ DESCRIPTION HERE """
-    return render_template('privacy_statement.html')
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """    
+    return render_template('privacy_statement.html')    
 
 #Starts the executing of program once program enters main.
 if __name__ == "__main__":
